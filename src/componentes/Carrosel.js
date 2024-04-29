7c6bec7ed27bc717b2f669e26b88944c794f1159
RodrigoMath <rodrigormathias@hotmail.com>
2024-04-28 21:57:31 -0300

    feature/AdicaoCarrousel

--- src/componentes/Carrosel.js
+++ src/componentes/Carrosel.js
@@ -1,7 +1,57 @@
-export function Carrosel(){
-    return(
-        <div>
-            <p>Aqui deve vir um Carrosel de Imagens, as imagens deve vir do backend(na apresentação)</p>
-        </div>
+import React, { useState } from "react";
+import styled from "styled-components";
+import vela1 from "../imagens/candly1.png"
+import vela2 from "../imagens/candly2.png"
+import vela3 from "../imagens/candly3.png"
+
+
+const CarouselContainer = styled.div`
+  width: 300px;
+  overflow: hidden;
+`;
+
+const CarouselTrack = styled.div`
+  display: flex;
+  transition: transform 0.5s ease;
+`;
+
+const CarouselItem = styled.img`
+  width: 100%;
+`;
+
+function Carousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    vela1,
+    vela2,
+    vela3
+  ];
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <CarouselContainer>
+      <CarouselTrack
+        style={{ transform: `translateX(-${currentIndex * 300}px)` }}
+      >
+        {images.map((image, index) => (
+          <CarouselItem key={index} src={image} alt={`Imagem ${index + 1}`} />
+        ))}
+      </CarouselTrack>
+      <button onClick={prevSlide}>Anterior</button>
+      <button onClick={nextSlide}>Próximo</button>
+    </CarouselContainer>
+  );
 }
+
+export default Carousel;
--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -1,15 +1,21 @@
-import { Carrosel } from "./Carrosel";
+import Carousel from "./Carrosel";
+import styled from "styled-components";
 
+const Central= styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
 export function Main(){
     return(
-        <>
-        <Carrosel></Carrosel>
+        <Central>
+        <Carousel></Carousel>
         <div> 
             <p>Aqui deve vir as outras imagens, que no futuro também vão vir do backend, abaixo de cada imagem deve ter o preço (vem do backend) e também a opção de adicionar ao carrinho</p>
             <p>Para estilizar, use os Styled Components, use o Header.js como exemplo.</p>
         </div>
-        </>
-
+        </Central>
     );
 }
