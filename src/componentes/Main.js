f18ca1dd0d177948a0a2072e4a75f04e238c7c86
RodrigoMath <rodrigormathias@hotmail.com>
2024-05-01 17:35:29 -0300

    [feature] - melhorias no carrousel

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -1,5 +1,6 @@
-import Carousel from "./Carrosel";
+import {Carrosel} from './Carrosel'
 import styled from "styled-components";
+import {motion} from 'framer-motion'
 
 const Central= styled.div`
     width: 100%;
@@ -8,10 +9,12 @@ const Central= styled.div`
     align-items: center;
 `
 
+
 export function Main(){
     return(
-        <Central>
-        <Carousel></Carousel>
+        <Central> 
+        <motion.h1 animate={{x: 200}} transition={{ duration: 1}}> Você diferente, velas diferentes </motion.h1>
+        <Carrosel></Carrosel>
         <div> 
             <p>Aqui deve vir as outras imagens, que no futuro também vão vir do backend, abaixo de cada imagem deve ter o preço (vem do backend) e também a opção de adicionar ao carrinho</p>
             <p>Para estilizar, use os Styled Components, use o Header.js como exemplo.</p>
