f33b3eca73a2424386b918f4cc94146ac0ab6e39
RodrigoMath <rodrigormathias@hotmail.com>
2024-05-26 21:56:25 -0300

    [feature]- adição dos 3 itens, para depois substituir as imagens, estilizar e ligar ao backend

--- src/componentes/Modal.js
+++ src/componentes/Modal.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import vela1 from "../imagens/candly1.png"
+import vela2 from "../imagens/candly2.png"
+import vela3 from "../imagens/candly3.png"
+
+const images = [
+  vela1,
+  vela2,
+  vela3
+]
 
 const ModalContainer = styled.div`
   position: fixed;
@@ -16,19 +25,42 @@ const ModalContainer = styled.div`
 const ModalContent = styled.div`
   background-color: white;
   padding: 20px;
+  width: 50%;
+  align-items: center;
+  justify-content: center;
   border-radius: 5px;
 `;
 
 const CloseButton = styled.button`
-  position: absolute;
   top: 10px;
   right: 10px;
   background: none;
-  background-color: black;
   border: none;
   font-size: 20px;
   cursor: pointer;
+ 
+  &::before {
+    content: '';
+    display: block;
+    width: 20px;
+    height: 20px;
+    background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAyNCAyNCI+PGxpbmUgY2xhc3M9ImxpbmUiIHgxOCIgeTE9IjYiIHgyPSI2IiB5Mj0iMThMCIvPjxsaW5lIGNsYXNzPSJsaW5lIiB4MT0iNiIgeTE9IjYiIHgyPSIxOCIgeTI9IjE4Ii8+PC9zdmc+');
+    background-repeat: no-repeat;
+    background-position: center;
+  }
 `;
+
+const ProdutosDoCarrinho = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+  height: 50%;
+`
+
+const ImagemProduto= styled.img`
+  width: 50%;
+  height: 50%;
+`
 
 export function Modal({ onCloseClick=null }) {
     function onClose(){
@@ -36,11 +68,32 @@ export function Modal({ onCloseClick=null }) {
             onCloseClick(false)
         }
     }
+
+   // Estado para o contador
+   const [counts, setCounts] = useState(Array(images.length).fill(0));
+
+   // Função para aumentar o contador
+   const handleIncrement = (index) => {
+     setCounts(prevCounts => {
+       const newCounts = [...prevCounts];
+       newCounts[index] = newCounts[index] + 1;
+       return newCounts;
+     });
+   };
   return (
     <ModalContainer>
       <ModalContent>
         <CloseButton onClick={onClose}>×</CloseButton>
         <h2>Compra Finalizada!</h2>
+        <ProdutosDoCarrinho>  
+          {images.map((image, index) => (
+             <div key={index}>
+             <ImagemProduto src={image} />
+             <p>Contador: {counts[index]}</p>
+             <button onClick={() => handleIncrement(index)}>Aumentar</button>
+           </div>
+            ))}     
+        </ProdutosDoCarrinho>
         <p>Obrigado por sua compra.</p>
       </ModalContent>
     </ModalContainer>
